99ed2d722ef095a82f9920c53cba3563793c20e0
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-04-03 16:53:31 -0300

    modelos de categoria e projeto

--- src/portfolio/admin.py
+++ src/portfolio/admin.py
@@ -1,1 +1,23 @@
+from django.contrib import admin
+
+from .models import Project, ProjectCategory
+
+
 # Register your models here.
+@admin.register(ProjectCategory)
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+    search_fields = ['name']
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'slug', 'description', 'category',
+        'image', 'live_link', 'github_link',
+        'created_at', 'updated_at'
+    )
+    list_filter = ('category', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ['title']
--- src/portfolio/models.py
+++ src/portfolio/models.py
@@ -1,1 +1,54 @@
+from django.db import models
+
+
 # Create your models here.
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nome')
+    slug = models.SlugField(verbose_name='Slug', unique=True)
+    description = models.TextField(blank=True, verbose_name='Descrição')
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Criado em',
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Atualizado em',
+    )
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Título')
+    slug = models.SlugField(verbose_name='Slug', unique=True)
+    description = models.TextField(blank=True, verbose_name='Descrição')
+    category = models.ForeignKey(
+        ProjectCategory,
+        on_delete=models.PROTECT,
+        verbose_name='Categoria'
+    )
+    image = models.ImageField(upload_to='projects/', verbose_name='Imagem')
+    live_link = models.URLField(blank=True, verbose_name='Link do Projeto')
+    github_link = models.URLField(blank=True, verbose_name='Link do Github')
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Criado em',
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Atualizado em',
+    )
+
+    class Meta:
+        verbose_name = 'Projeto'
+        verbose_name_plural = 'Projetos'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
