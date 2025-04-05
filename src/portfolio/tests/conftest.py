2964439ff922f4e824c6fc04ca33a7db423e263c
Matheus Fabião <matheusfabiao.dev@gmail.com>
2025-04-05 18:30:56 -0300

    add docstrings em conftest.py

--- src/portfolio/tests/conftest.py
+++ src/portfolio/tests/conftest.py
@@ -5,6 +5,11 @@ from portfolio.models import Project, ProjectCategory
 
 @pytest.fixture
 def sample_project_category():
+    """ Exemplo de categoria de projeto
+
+    Returns:
+        ProjectCategory : Categoria de projeto
+    """
     return ProjectCategory.objects.create(
         name='Categoria de Projeto',
         slug='categoria-de-projeto',
@@ -14,6 +19,11 @@ def sample_project_category():
 
 @pytest.fixture
 def sample_project():
+    """ Exemplo de projeto
+
+    Returns:
+        Project : Projeto
+    """
     return Project.objects.create(
         title='Project 1',
         slug='project-1',
